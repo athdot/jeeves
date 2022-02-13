dfc0c8eb7e572e46b985e4b77009a9669602249a
athdot <athdot@gmail.com>
2022-02-12 19:30:55 -0500

    better long-short changes

--- lib/lpLongShort.py
+++ lib/lpLongShort.py
@@ -17,6 +17,14 @@ class TradeAlgo:
         
         stockUniverse = os.environ["STOCK_UNIVERSE"].split(",")
         
+        # Format the allStocks variable for use in the class.
+        self.allStocks = []
+        for stock in stockUniverse:
+            self.allStocks.append([stock, 0])
+        
+        self.timeToClose = None
+        self.opening = True
+        
     def run(self):
         utils.p_error("NOTIFICATION: RUNNING A LOW-POWER LONG-SHORT ALGO")
         utils.p_sep()
@@ -27,6 +35,35 @@ class TradeAlgo:
         tAMO.start()
         tAMO.join()
         print("Market opened.")
+        
+        # Rebalance the portfolio every minute, making necessary trades.
+        while True:
+            # Figure out when the market will close so we can prepare to sell beforehand.
+            clock = self.alpaca.get_clock()
+            closingTime = clock.next_close.replace(tzinfo=datetime.timezone.utc).timestamp()
+            currTime = clock.timestamp.replace(tzinfo=datetime.timezone.utc).timestamp()
+            self.timeToClose = closingTime - currTime
+            
+            if(self.timeToClose < (60 * 15)):
+                # Run script again after market close for next trading day.
+                print("Sleeping until market close (15 minutes).")
+                time.sleep(60 * 15)
+                
+                self.opening = True
+        
+                print("Waiting for market to open...")
+                tAMO = threading.Thread(target=self.awaitMarketOpen)
+                tAMO.start()
+                tAMO.join()
+                print("Market opened.")
+            else:
+                # Rebalance the portfolio, and do stuff for the day
+                # Change self.opening to False once we remove old stocks
+                
+                # tRebalance = threading.Thread(target=self.rebalance)
+                # tRebalance.start()
+                # tRebalance.join()
+                time.sleep(60)
         
         
     # Wait for market to open.
