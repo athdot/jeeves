45c193f84c8648dd074219c39c0e923ae41b17ad
athdot <athdot@gmail.com>
2022-02-19 23:26:59 -0500

    Test algo changes, and candlestick analysis

--- lib/testBed.py
+++ lib/testBed.py
@@ -25,14 +25,15 @@ class TradeAlgo:
         utils.p_error("TEST FUNCTION RUNNER")
         utils.p_sep()
         
+        # self.run_candle_test()
         self.run_tests(30)
-        
+
         utils.p_sep()
         utils.p_error("OPERATION COMPLETE")
         utils.p_sep()
         
     def run_tests(self, period):
-        run_num = 4
+        run_num = 3 # 4
         
         # Header
         print("Day-Period: " + str(run_num))
@@ -57,8 +58,11 @@ class TradeAlgo:
                 local_bars = self.get_bars(stock_name, run_num, day + weekend_offset)
                 
                 # Generate prediction
-                local_perdiction = self.trend_line(local_bars)
+                local_perdiction = self.simple_dir(local_bars)
                 
+                # Flip, betting on volitility
+                local_perdiction = -local_perdiction
+
                 if local_perdiction > 0:
                     local_perdiction = "P"
                 elif local_perdiction == 0:
@@ -110,7 +114,6 @@ class TradeAlgo:
         
         return bars
         
-
     def trend_line(self, bars):
         # Trendline
         mean_val_y = list()
@@ -144,3 +147,163 @@ class TradeAlgo:
             temp.append(100 * (mean_val_y[val + 1] - mean_val_y[val]) / mean_val_y[val])
         
         return sum(temp) / len(temp)
+
+    def simple_dir(self, bars):
+        mean_val_y = []
+        for bar in bars:
+            mean_val_y.append((bar.o + bar.c + bar.h + bar.l) / 4)
+
+        # Convert to precentages
+        temp = list()
+        for val in range(0, len(mean_val_y) - 1):
+            temp.append(100 * (mean_val_y[val + 1] - mean_val_y[val]) / mean_val_y[val])
+        
+        return sum(temp) / len(temp)
+        
+        return bars[len(bars) - 1].c - bars[len(bars) - 1 - day_period].o
+
+    def run_candle_test(self):
+        day_period = 10 # 15 for funsies, 10 for trends
+        start_day = 0
+        stock_name = "DOMO"
+
+        # Header
+        print("Day-Period: " + str(day_period))
+        print("Function to Test: candle_analysis")
+        print("Current Stock: " + stock_name)
+        utils.p_sep()
+
+        local_bars = self.get_bars(stock_name, day_period, start_day)
+
+        local_perdiction = self.candle_analysis(local_bars)
+
+
+    def candle_analysis(self, bars):
+        # This function takes our inputted bars, and does a number of checks to find
+        # Patterns within a period of X days
+        # Outputs from this are supposed to follow this format
+        #
+        # p is pressure, True is up, False is down
+        # r is relevance (how recent is it, lower is better) number of days essentially
+        #
+        # As of now, this is what we are checking for:
+        # - Hammer & Reverse Hammer, Hanging man & Shooting star - DONE (Need better trend algo?)
+        # - Bullish engulfing, Bearish engulfing - DONE (Need better trend algo?)
+        # - Piercing line - DONE (Idk how id test this lol)
+        # - Morning star, Evening star
+        # - Three white soldiers, Three black crows
+        # - Dark cloud cover
+        #
+        # Perhaps implement:
+        # - Doji
+        # - Spinning top
+        # - Falling three methods, Rising three methods
+
+        output_list = []
+
+        # Check recent history for any evidence of a hammer, and if exists, add it to the output list
+
+        # Bullish hammer viewing
+        temp = [] # Right here, its a list of potential hammers within the study
+        p_crop = 0.35 # Limiter for a hammer
+        day_period = 6 # How many days prior we look for trends
+
+        # HAMMERS
+        for bar in range(0, len(bars)):
+            c_bar = bars[bar]
+
+            # Classify as a hammer if body is above/below 35% of height
+            low = c_bar.o
+            high = c_bar.c
+            if c_bar.o > c_bar.c:
+                low = c_bar.c
+                high = c_bar.o
+
+            if low > (c_bar.h - c_bar.l) * (1 - p_crop) + c_bar.l or high < (c_bar.h - c_bar.l) * p_crop + c_bar.l:
+                # Qualifies as a hammer/star of some kind
+                temp.append(bar)
+
+        # TODO("If slope is too small, dont decide?")
+
+        for val in temp:
+            # Go through each identified hammer and see where the trend is headed
+            # Period of 4 prior days
+            rev_day = val - day_period
+            
+            if rev_day < 0:
+                continue
+
+            short_bars = bars[rev_day:val]
+
+            # Trend line seems to preform well at 4 days
+            # delta pctg seems to do well at 6
+            trend = self.delta_pctg(short_bars)
+
+            output_list.append({"t":"hammer","p":(trend < 0),"r":(len(bars) - val)})
+
+        # ENGULFING
+        for bar in range(1, len(bars)):
+            p_bar = bars[bar - 1]
+            c_bar = bars[bar]
+
+            rev_day = bar - day_period
+
+            if rev_day < 0:
+                continue
+
+            short_bars = bars[rev_day:bar]
+
+            if c_bar.h > p_bar.h and c_bar.l < p_bar.l:
+                if p_bar.o < p_bar.c:
+                    if c_bar.o > c_bar.c:
+                        # Bearish engulfing
+                        # Check prior trend, if its positive, this means a possible reversal
+                        trend = self.delta_pctg(short_bars)
+                        if trend > 0:
+                            # We have it!
+                            output_list.append({"t":"engulfing","p":False,"r":(len(bars) - bar)})
+                else:
+                    if c_bar.o < c_bar.c:
+                        # Bullish engulfing
+                        # Check prior trend, if its negative, this means a possible reversal
+                        trend = self.delta_pctg(short_bars)
+                        if trend < 0:
+                            # We have it!
+                            output_list.append({"t":"engulfing","p":True,"r":(len(bars) - bar)})
+        
+        # PIERCING_LINE
+        # Average bar size
+        delta_b = 0
+        for bar in bars:
+            delta_b = delta_b + abs(bar.o - bar.c)
+        delta_b = delta_b / len(bars)
+
+        res = (sum([((abs(bar.o - bar.c) - delta_b) ** 2) for bar in bars]) / len(bars)) ** 0.5
+
+        for bar in range(1, len(bars)):
+            p_bar = bars[bar - 1]
+            c_bar = bars[bar]
+
+            # Is the bar size applicable?
+            p_z_score = (abs(p_bar.o - p_bar.c) - delta_b) / res
+            c_z_score = (abs(c_bar.o - c_bar.c) - delta_b) / res
+            
+            # 0.85 is about 80% of all candles
+            z_limit = 0.85
+            if p_z_score < z_limit or c_z_score < z_limit:
+                continue
+
+            if p_bar.o > p_bar.c and c_bar.o < c_bar.c and p_bar.c > c_bar.o and c_bar.c > (p_bar.o - p_bar.c) / 2 + p_bar.c:
+                output_list.append({"t":"piercing","p":True,"r":(len(bars) - bar)})
+
+        # STAR
+        # TODO("Other patterns")
+
+        # DEFAULT
+        # Last 2 days sloping?
+        day_period = 3
+        output_list.append({"t":"default","p":(bars[len(bars) - 1].c - bars[len(bars) - 1 - day_period].o > 0),"r":0})
+
+        print(output_list)
+
+        return output_list
